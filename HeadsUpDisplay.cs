bbcb81e9048249bba547f5c26201d31c21996b85
Costa <CostaHan@outlook.com>
2026-06-30 22:37:51 +0800

    v3

--- HeadsUpDisplay.cs
+++ HeadsUpDisplay.cs
@@ -15,8 +15,8 @@ public partial class HeadsUpDisplay : CanvasLayer
 	
 	public void _on_start_button_button_down() {
 		// 三个 UI 节点都嵌套在 WelcomePageVLayout 下，当前脚本就在 HeadsUpDisplay 上
-		GetNode<Control>("WelcomePageVLayout").Hide();
-
+		// GetNode<Control>("WelcomePageVLayout").Hide();
+		Hide();
 		// 委托给 Main 统一管理开始游戏逻辑（读取 StartPosition、启动玩家等）
 		GetParent<Main>().NewGame();
 	}
